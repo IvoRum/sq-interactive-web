64e34d118da1dd6753501ca6018695c4df266a9e
IvoRum <ivailo.rumenov@outlook.com>
2026-09-10 15:31:13 +0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <h1>SQ Interactive</h1>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+</style>
